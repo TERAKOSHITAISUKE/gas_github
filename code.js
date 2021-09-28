eef863e2c8ddbe4bac5e387f46c43694cc4266ec
Tera <denpsyadvance@gmail.com>
2021-09-28 21:11:57 +0900

    first

---
+++ code.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  console.log("hello world")
+}
+
+//ENOENT: no such file or directory, open 'C:\Users\denps\.clasprc.json'
+
+function tera2_1() {
+    class Person{
+      constructor(height, weight) {
+        this.height = height;
+        this.weight = weight;
+      }
+    }
+    const tera = new Person(165, 60);
+    console.log(tera, tera.height, tera.weight);//{ height: 165, weight: 60 } 165 60
+    
+}
