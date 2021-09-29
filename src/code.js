4f9ffde662a4a171a0f35b13a28b418e5f09c4ca
Tera <denpsyadvance@gmail.com>
2021-09-29 15:45:46 +0900

    forth

--- src/code.js
+++ src/code.js
@@ -23,10 +23,11 @@ function tera2_2(){
         this.name = name;
         this.price = price;
         this.stock = stock;
-    }
+      }
+   };
    const item = new Onigiri('梅', 120, 5);
-   console.log(item.name);
-   console.log(item.price);
-   console.log(item.stock);
-  };
+   console.log(item.name, item.price, item.stock);//梅 120 5
+
+   const item2 = new Onigiri('昆布', 90, 20);
+   console.log(item2.name, item2.price, item2.stock);//昆布 90 20
 }
