e246382e6e5202042af5971760458579d12e7063
Tera <denpsyadvance@gmail.com>
2021-09-29 15:25:09 +0900

    third

--- src/code.js
+++ src/code.js
@@ -15,3 +15,18 @@ function tera2_1() {
     console.log(tera, tera.height, tera.weight);//{ height: 165, weight: 60 } 165 60
     
 }
+
+
+function tera2_2(){
+   class Onigiri{
+      constructor(name, price, stock){
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+   const item = new Onigiri('梅', 120, 5);
+   console.log(item.name);
+   console.log(item.price);
+   console.log(item.stock);
+  };
+}
