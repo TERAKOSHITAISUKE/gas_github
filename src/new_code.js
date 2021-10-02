bc3e217c4d0bedf0ca77ae04262a3dca18fee0f1
Tera <denpsyadvance@gmail.com>
2021-10-02 10:46:34 +0900

    homework 5

--- src/new_code.js
+++ src/new_code.js
@@ -1,3 +1,28 @@
-function myFunction() {
-  
+function tera2_5() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('シート2');
+  const data = sheet.getDataRange().getValues();
+  data.shift();
+
+  class Onigiri {
+    constructor(name, price, stock) {
+      this.name = name;
+      this.price = price;
+      this.stock = stock;
+    }
+    getTaxIncluded(tax = 0.1) {
+      return this.price * (1 + tax)
+    }
+    isSoldOut() {
+      if (this.stock > 0) {
+        return 'true';
+      } else {
+        return 'false';
+      }
+    }
+  };
+
+  const item = new Onigiri('梅', 100, 50);
+  console.log(item);                      //{ name: '梅', price: 100, stock: 50 }
+  console.log(item.getTaxIncluded(0.08)); //108
+  console.log(item.isSoldOut());          //true
 }
